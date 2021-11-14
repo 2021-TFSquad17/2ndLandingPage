0552e8e7f796742f0ea2cc796b2619523236f9e6
Rafael Noll <rafanoll2004@gmail.com>
2021-11-14 11:07:10 -0300

    Atualizando componentização da secção 'Apoiadores'

--- project/src/components/Apoiadores/Apoiadores.jsx
+++ project/src/components/Apoiadores/Apoiadores.jsx
@@ -1,29 +1,27 @@
-import { Component } from "react";
-import ClientImage from "../../assets/img/logos/microsoft.svg";
+/*Imagens Apoiadores */
+import imagemApoiador1 from "../../assets/img/logos/facebook.svg";
+import imagemApoiador2 from "../../assets/img/logos/google.svg";
+import imagemApoiador3 from "../../assets/img/logos/microsoft.svg";
+import LogoApoiadores from "./LogoApoiadores/LogoApoiadores";
 
-class Apoiadores extends Component {
-    render() {
-        return (
-            <div className="py-5">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-md-3 col-sm-6 my-3">
-                            <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={ClientImage} alt="..." /></a>
-                        </div>
-                        <div className="col-md-3 col-sm-6 my-3">
-                            <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={ClientImage} alt="..." /></a>
-                        </div>
-                        <div className="col-md-3 col-sm-6 my-3">
-                            <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={ClientImage} alt="..." /></a>
-                        </div>
-                        <div className="col-md-3 col-sm-6 my-3">
-                            <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={ClientImage} alt="..." /></a>
-                        </div>
-                    </div>
+function Apoiadores() {
+    const imagensApoiadores = [
+        imagemApoiador1,
+        imagemApoiador2,
+        imagemApoiador3
+    ];
+    
+    return (
+        <div className="py-5">
+            <div className="container">
+                <div className="row align-items-center">
+                    {imagensApoiadores.map((imagem, index) => {
+                        return <LogoApoiadores imagemLogo={imagem} key={index} />
+                    })}
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Apoiadores;
---
+++ project/src/components/Apoiadores/LogoApoiadores/LogoApoiadores.jsx
@@ -0,0 +1,9 @@
+function LogoApoiadores({ imagemLogo }) {
+    return (
+        <div className="col-md-3 col-sm-6 my-3">
+            <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src={imagemLogo} alt="..." /></a>
+        </div>
+    );
+}
+
+export default LogoApoiadores;
