88b0f23e47fd7a9504f6ec6d9325c24ea4534bf7
Rafael Noll <rafanoll2004@gmail.com>
2021-11-15 18:18:08 -0300

    Adicionando pasta com fotos dos membros do time

--- project/src/components/Time/MembroDoTime.jsx
+++ project/src/components/Time/MembroDoTime.jsx
@@ -1,11 +1,10 @@
-import TeamImage from "../../assets/img/team/1.jpg";
 import "./style.css";
 
-function MembroDoTime({nome,github,linkedin}) {
+function MembroDoTime({nome,github,linkedin,fotoMembro}) {
     return (
         <div className="col-lg-2-5">
             <div className="team-member">
-                <img className="mx-auto rounded-circle" src={TeamImage} alt="..." />
+                <img className="mx-auto rounded-circle" src={fotoMembro} alt="Imagem membro do time" />
                 <h4>{nome}</h4>
                 <a className="btn btn-dark btn-social mx-2" href={github}><i className="fab fa-github"></i></a>
                 <a className="btn btn-dark btn-social mx-2" href={linkedin}><i className="fab fa-linkedin-in"></i></a>
--- project/src/components/Time/Time.jsx
+++ project/src/components/Time/Time.jsx
@@ -1,35 +1,62 @@
 import Carousel from "../../../../node_modules/react-grid-carousel";
 import MembroDoTime from "./MembroDoTime";
 
+import fotoEmellyn from "../../assets/img/fotos-time/foto-emellyn.jpg";
+import fotoJuliana from "../../assets/img/fotos-time/foto-juliana.jpg";
+import fotoKelly from "../../assets/img/fotos-time/foto-kelly.jpg";
+import fotoLaura from "../../assets/img/fotos-time/foto-laura.jpg";
+import fotoLeonardo from "../../assets/img/fotos-time/foto-leonardo.jpg";
+import fotoMarina from "../../assets/img/fotos-time/foto-marina.jpg";
+import fotoMurilo from "../../assets/img/fotos-time/foto-murilo.jpg";
+import fotoNatalie from "../../assets/img/fotos-time/foto-natalie.jpg";
+import fotoRafael from "../../assets/img/fotos-time/foto-rafael.jpg";
+import fotoThales from "../../assets/img/fotos-time/foto-thales.jpg";
+
 function Time() {
+    const fotosTime = [
+        fotoEmellyn,
+        fotoJuliana,
+        fotoKelly,
+        fotoLaura,
+        fotoLeonardo,
+        fotoMarina,
+        fotoMurilo,
+        fotoNatalie,
+        fotoRafael,
+        fotoThales
+    ];
+
     const dadosTime = {
-        membro1: { nome: "Membro1", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro2: { nome: "Membro2", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro3: { nome: "Membro3", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro4: { nome: "Membro4", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro5: { nome: "Membro5", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro6: { nome: "Membro6", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro7: { nome: "Membro7", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro8: { nome: "Membro8", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro9: { nome: "Membro9", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
-        membro10: { nome: "Membro10", github: "https://github.com/", linkedin: "https://br.linkedin.com/" },
+        membro1: { nome: "Emellyn", github: "https://github.com/EmellynRosa", linkedin: "https://www.linkedin.com/in/emellynrosa/"},
+        membro2: { nome: "Juliana", github: "https://github.com/julianaa-f", linkedin: "https://www.linkedin.com/in/julianaa-f"},
+        membro3: { nome: "Kelly", github: "https://github.com/wtkellyy", linkedin: "https://www.linkedin.com/in/kellyrramos/"},
+        membro4: { nome: "Laura", github: "https://github.com/lauramesquita", linkedin: "https://www.linkedin.com/in/laura-mesquita-20bab6220/"},
+        membro5: { nome: "Leonardo", github: "https://github.com/LeonardodsMarques", linkedin: "https://www.linkedin.com/in/leonardo-dsmarques/"},
+        membro6: { nome: "Marina", github: "https://github.com/MarinaRaelMarques", linkedin: "https://www.linkedin.com/in/marina-rael-marques/"},
+        membro7: { nome: "Murilo", github: "https://github.com/Murilo-Barbosa", linkedin: "http://linkedin.com/in/murilo-barbosa-a58164216"},
+        membro8: { nome: "Natalie", github: "https://github.com/NatalieLopes", linkedin: "https://www.linkedin.com/in/natalie-ds-lopes?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMxL4ygaHQiGD68r%2FbgyMmg%3D%3D"},
+        membro9: { nome: "Rafael", github: "https://github.com/Rafaelnoll", linkedin: "https://www.linkedin.com/in/rafael-noll-da-silva-8a0a6521b/"},
+        membro10: { nome: "Thales", github: "https://github.com/OdiasThales23", linkedin: "https://www.linkedin.com/in/thales-dias-prudencio-670538220"},
     }
 
     const membrosDoTime = [
         <MembroDoTime
             nome={dadosTime.membro1.nome}
             github={dadosTime.membro1.github}
-            linkedin={dadosTime.membro1.linkedin} />,
+            linkedin={dadosTime.membro1.linkedin}
+            />,
 
         <MembroDoTime
             nome={dadosTime.membro2.nome}
             github={dadosTime.membro2.github}
-            linkedin={dadosTime.membro2.linkedin} />,
+            linkedin={dadosTime.membro2.linkedin}
+            />,
 
         <MembroDoTime
             nome={dadosTime.membro3.nome}
             github={dadosTime.membro3.github}
-            linkedin={dadosTime.membro3.linkedin} />,
+            linkedin={dadosTime.membro3.linkedin} 
+            />,
 
         <MembroDoTime
             nome={dadosTime.membro4.nome}
